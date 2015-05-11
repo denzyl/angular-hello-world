81b842e3fc40b6ed2f792ac7e240b47a107886d3
Denzyl Balram <denzyl.balram@gmail.com>
2015-05-11 15:59:17 -0700

    Add examples from ng-book

--- app.js
+++ app.js
@@ -8,4 +8,22 @@ angular.module('myApp', [])
       }, 1000);
     };
   updateClock();
-});
+  
+})
+  .controller('FirstController', function($scope) {
+    $scope.counter = 0;
+    $scope.add = function(amount) { $scope.counter += amount; };
+    $scope.subtract = function(amount) { $scope.counter -= amount; };
+})
+
+   .controller('ParentController', function($scope) { 
+    	$scope.person = {greeted: true};
+    })
+    
+   .controller('ChildController', function($scope) { 
+      $scope.sayHello = function() {
+        $scope.person.name = "Denzyl Balram";
+      };
+    });
+
+  
